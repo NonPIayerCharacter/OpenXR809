d822dcf1b22bb321a27b0e2edbf41f0de3e4ca94
divadiow <divadiow@hotmail.com>
2025-02-28 16:52:25 +0000

    revert initial change in cmd_dhcpd.c to see if both places are needed ot just dhcpd_cfg.h

--- project/common/cmd/cmd_dhcpd.c
+++ project/common/cmd/cmd_dhcpd.c
@@ -34,8 +34,8 @@
 #include "net/udhcp/usr_dhcpd.h"
 #include "lwip/inet.h"
 
-#define CMD_DHCPD_ADDR_START "192.168.4.100"
-#define CMD_DHCPD_ADDR_END   "192.168.4.150"
+#define CMD_DHCPD_ADDR_START "192.168.51.100"
+#define CMD_DHCPD_ADDR_END   "192.168.51.150"
 
 static struct dhcp_server_info dhcpd_info;
 
